1c0f444f97f4df303b2494dc33691ed4aacbbd28
MaximilianoEscrich <139141261+MaximilianoEscrich@users.noreply.github.com>
2023-09-04 07:52:53 -0600

    Add files via upload

---
+++ js/SweetAlert.js
@@ -0,0 +1,19 @@
+function Mostrar(){ 
+
+    Swal.fire(
+        'Gracias por enviar tú mensaje!',
+        'Te contestaremos cuando antes!',
+        'success'
+      )
+      
+}
+
+function Membresia(){ 
+
+    Swal.fire(
+        'Gracias por obtener nuestra membresía!',
+        'Sigma + aún está en desarollo, pero te mantendremos al tanto!',
+        'success'
+      )
+      
+}
